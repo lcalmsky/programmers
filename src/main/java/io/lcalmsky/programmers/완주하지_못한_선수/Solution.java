06393fb0b9e37238263a320a1967b91860e5ce67
Jaime <lcalmsky@gmail.com>
2020-11-20 17:51:11 +0900

    두 문제 해결

---
+++ src/main/java/io/lcalmsky/programmers/완주하지_못한_선수/Solution.java
@@ -0,0 +1,14 @@
+package io.lcalmsky.programmers.완주하지_못한_선수;
+
+import java.util.Arrays;
+
+public class Solution {
+    public String solution(String[] participant, String[] completion) {
+        Arrays.sort(participant);
+        Arrays.sort(completion);
+        for (int i = 0; i < participant.length - 1; i++) {
+            if (!participant[i].equals(completion[i])) return participant[i];
+        }
+        return participant[participant.length - 1];
+    }
+}
